d56447948d23b215bed05bfe1201ba056822aecb
Alan38345 <alaxddalan@gmail.com>
2025-10-30 21:37:59 +0100

    mnozenie

--- kalkulator.py
+++ kalkulator.py
@@ -41,10 +41,35 @@ def potegowanie(a, b):
 
 komenda = input('podaj komende  ')
 if komenda == 'dodawanie':
-      a = float(input('pierwsza liczba  '))
-      b = float(input('druga liczba  '))
+      a = float(input('podaj pierwszy składnik  '))
+      b = float(input('podaj drugi składnik  '))
       dodawanie(a, b)
       suma = dodawanie(a, b)
-      print(suma)
+      print(f'twoj wynik to {suma}')
+elif komenda == 'mnożenie':
+    a = float(input('podaj pierwszy czynnik  '))
+    b = float(input('podaj drugi czynnik '))
+    iloczyn = mnożenie(a, b)
+    odp = float(input('jesli chcesz podac jeszcze kolejny czynnik wcisnij 1 jesli nie wcisnij 2 '))
+    if odp == 1:
+        c = float(input('podaj trzeci skladnik '))
+        iloczyn *= c
+        odp2 = float(input('jesli chcesz podac jeszcze kolejny czynnik wcisnij 1 jesli nie wcisnij 2 '))
+        if odp2 == 1:
+            d = float(input('podaj czwarta liczbe '))
+            iloczyn *= d
+            odp3 = float(input('ostatni raz mozesz dodac jeszcze jednen czynnik jesli chcesz to uczynic kliknij 1 jesli nie 2 '))
+            if odp3 == 1:
+                e = float(input('podaj ostatni czynnik '))
+                iloczyn *= e
+                print('mozesz dodac maksymalnie 5 czynnikow, więc: ')
+    iloczyn = int(iloczyn)
+    print(f'wynik twojego mnozenia to  {iloczyn}' )
+
+
+
+
+
+input('nacisnij enter zeby zakonczyc')
 
 
