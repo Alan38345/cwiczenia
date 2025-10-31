22ef92cde1296c2b7072da935bf04f61976830fa
Alan38345 <alaxddalan@gmail.com>
2025-10-31 19:59:11 +0100

    uproszczenie

--- kalkulator.py
+++ kalkulator.py
@@ -1,8 +1,60 @@
+komenda = input('podaj komende ')
 
-dzialanie = input('wcisnij pytajnik by dowiedziec sie jakie sa komendy? ')
-if dzialanie == '?':
-    print("""O to komendy
-          dodawanie
+komendy =["dodawanie",
+          "odejmowanie",
+          "mnożenie",
+          "dzielenie",
+          "potęgowanie",
+          "pierwiastkowanie",
+          "liczenie procentów",
+          "logarytmy",
+          "wspólny punkt dwóch prostych",
+          "liczby pierwsze",
+          "nwd",
+          "nww",
+          "?"]
+
+
+
+
+
+
+def dodawanie(a, b):
+      suma = a+b
+      return suma
+def odejmowanie(a,b ):
+      różnica = a-b
+      return różnica
+def dzielenie(a, b):
+      iloraz = a/b
+      return iloraz
+def potegowanie(a, b):
+    potega = a**b
+    return potega
+
+if komenda in komendy:
+    if komenda == 'dodawanie':
+      a = float(input('podaj pierwszy składnik  '))
+      b = float(input('podaj drugi składnik  '))
+      dodawanie(a, b)
+      suma = dodawanie(a, b)
+      print(f'twoj wynik to {suma}')
+    elif komenda == 'mnożenie':
+        obecnaliczba = 0
+        ilosc = int(input('ile liczb chcesz pomnożyć przez siebie?  '))
+        wynik = 1
+        for y in range(ilosc):
+            obecnaliczba += 1
+            liczba = int(input(f'podaj liczbe numer {obecnaliczba} '))
+            wynik *= liczba
+        print(f'wynik twojego mnozenia to  {wynik}' )
+    elif komenda == 'odejmowanie':
+        a = float(input('podaj odjemną'))
+        b = float(input('podaj odjemnik'))
+        roznica = odejmowanie(a, b)
+        print(f'wynikiem odejmowanie jest {roznica}')
+    elif komenda == "?":
+        print("""dodawanie
           odejmowanie
           mnożenie
           dzielenie
@@ -14,66 +66,13 @@ if dzialanie == '?':
           liczby pierwsze
           nwd
           nww""")
-else:
-    print('nie kojarze tej komendy')
-
-
-def dodawanie(a, b):
-      suma = a+b
-      return suma
-def mnożenie(a, b):
-      iloczyn = a*b
-      return iloczyn
-def odejmowanie(a,b ):
-      różnica = a-b
-      return różnica
-def dzielenie(a, b):
-      iloraz = a/b
-      return iloraz
-def potegowanie(a, b):
-    liczba = 1
-    for x in range(b):
-        liczba *= a
-    return liczba
-
-
-
-
-komenda = input('podaj komende  ')
-if komenda == 'dodawanie':
-      a = float(input('podaj pierwszy składnik  '))
-      b = float(input('podaj drugi składnik  '))
-      dodawanie(a, b)
-      suma = dodawanie(a, b)
-      print(f'twoj wynik to {suma}')
-elif komenda == 'mnożenie':
-    a = float(input('podaj pierwszy czynnik  '))
-    b = float(input('podaj drugi czynnik '))
-    iloczyn = mnożenie(a, b)
-    odp = float(input('jesli chcesz podac jeszcze kolejny czynnik wcisnij 1 jesli nie wcisnij 2 '))
-    if odp == 1:
-        c = float(input('podaj trzeci skladnik '))
-        iloczyn *= c
-        odp2 = float(input('jesli chcesz podac jeszcze kolejny czynnik wcisnij 1 jesli nie wcisnij 2 '))
-        if odp2 == 1:
-            d = float(input('podaj czwarta liczbe '))
-            iloczyn *= d
-            odp3 = float(input('ostatni raz mozesz dodac jeszcze jednen czynnik jesli chcesz to uczynic kliknij 1 jesli nie 2 '))
-            if odp3 == 1:
-                e = float(input('podaj ostatni czynnik '))
-                iloczyn *= e
-                print('mozesz dodac maksymalnie 5 czynnikow, więc: ')
-    iloczyn = int(iloczyn)
-    print(f'wynik twojego mnozenia to  {iloczyn}' )
-elif komenda == 'odejmowanie':
-    a = float(input('podaj odjemną'))
-    b = float(input('podaj odjemnik'))
-    roznica = odejmowanie(a, b)
-    print(f'wynikiem odejmowanie jest {roznica}')
+    else:
+        print('cos slaba komenda')
 
 
 
 
 input('nacisnij enter zeby zakonczyc')
+
 
 
