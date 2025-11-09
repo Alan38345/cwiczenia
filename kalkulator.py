4dd1471f258f81c4255d9f2ba5624e0ea5a4b5ba
Alan38345 <alaxddalan@gmail.com>
2025-11-09 09:33:20 +0100

    przerobienie tego w nieskonczony program

--- kalkulator.py
+++ kalkulator.py
@@ -1,4 +1,5 @@
-command = input('write a command ')
+import sys
+
 
 commands = [
     "summing",
@@ -29,53 +30,76 @@ def division(a, b):
 def exponentiation(a, b):
     power = a ** b
     return power
+answer = 'yes'
 
-if command in commands:
-    if command == 'summing':
-        a = float(input('Provide the first addend: '))
-        b = float(input('Provide the second addend: '))
-        summing(a, b)
-        summing = summing(a, b)
-        print(f'Your result is {summing}')
-    elif command == 'multiplication':
-        current_number = 0
-        amount = int(input('How many numbers do you want to multiply? '))
-        result = 1
-        for y in range(amount):
-            current_number += 1
-            number = int(input(f'Enter number {current_number}: '))
-            result *= number
-        print(f'Your multiplication result is {result}')
-    elif command == 'subtraction':
-        a = float(input('Enter the minuend: '))
-        b = float(input('Enter the subtrahend: '))
-        difference = subtraction(a, b)
-        print(f'The result of subtraction is {difference}')
-    elif command == "?":
-        print("""addition
-          subtraction
-          multiplication
-          division
-          exponentiation
-          root extraction
-          percentage calculation
-          logarithms
-          intersection point of two lines
-          prime numbers
-          gcd
-          lcm""")
-    elif command == "division":
-        divisor = float(input('Enter the divisor: '))
-        amount = int(input('Enter how many numbers you want to divide by this number: '))
-        z = 0
-        for x in range(1, amount+1):
-            z += 1
-            y = float(input(f'write a {z}. number that you want to devide by '))
-            divisor /= y
+while answer == 'yes':
+    command = input('write a command ')
+    if command in commands:
 
-        print(f'The result of the division is: {divisor}')
-    else:
-        print('Something is wrong with the command')
+        if command == "?":
+            print("""
+                addition
+                subtraction
+                 multiplication
+                division
+                exponentiation
+                root extraction
+                percentage calculation
+                logarithms
+                intersection point of two lines
+                prime numbers
+                gcd
+                lcm""")
+        if command == 'summing':
+            a = float(input('Provide the first addend: '))
+            b = float(input('Provide the second addend: '))
+            summing(a, b)
+            summing = summing(a, b)
+            print(f'Your result is {summing}')
+        elif command == 'multiplication':
+            current_number = 0
+            amount = int(input('How many numbers do you want to multiply? '))
+            result = 1
+            for y in range(amount):
+                current_number += 1
+                number = int(input(f'Enter number {current_number}: '))
+                result *= number
+            print(f'Your multiplication result is {result}')
+        elif command == 'subtraction':
+            a = float(input('Enter the minuend: '))
+            b = float(input('Enter the subtrahend: '))
+            difference = subtraction(a, b)
+            print(f'The result of subtraction is {difference}')
+        elif command == "?":
+            print("""
+            addition
+            subtraction
+            multiplication
+            division
+            exponentiation
+            root extraction
+            percentage calculation
+            logarithms
+            intersection point of two lines
+            prime numbers
+            gcd
+            lcm""")
+        elif command == "division":
+            divisor = float(input('Enter the divisor: '))
+            amount = int(input('Enter how many numbers you want to divide by this number: '))
+            z = 0
+            for x in range(1, amount+1):
+                z += 1
+                y = float(input(f'write a {z}. number that you want to devide by '))
+                if y==0:
+
+                    sys.exit("nawet nie probuj gagatku")
+                    divisor /= y
+                    print(f'The result of the division is: {divisor}')
+        else:
+            print('Something is wrong with the command')
+        answer = ''
+        answer = input('Do you want to continue?')
 
 input('Press enter to exit')
 
