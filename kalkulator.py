0496d813a3868fcf81c1550f6f04c860f045de4a
Alan38345 <alaxddalan@gmail.com>
2025-11-10 22:55:09 +0100

    uzycie starej liczby

--- kalkulator.py
+++ kalkulator.py
@@ -32,70 +32,149 @@ def exponentiation(a, b):
     power = a ** b
     return power
 answer = 'yes'
+secque = 'no'
 
-while answer == 'yes':
-    command = input('write a command (if you dont know any commands write "?" ')
-    if command in commands:
+while True:
+    if answer == 'yes' and secque == 'no':
+        result = ""
+        command = input('write a command (if you dont know any commands write "?" ')
+        if command in commands:
+            if command == "?":
+                print("""
+                    addition
+                    subtraction
+                    multiplication
+                    division
+                    exponentiation
+                    root extraction
+                    percentage calculation
+                    logarithms
+                    intersection point of two lines
+                    prime numbers
+                    gcd
+                    lcm""")
 
-        if command == "?":
-            print("""
-                addition
-                subtraction
-                multiplication
-                division
-                exponentiation
-                root extraction
-                percentage calculation
-                logarithms
-                intersection point of two lines
-                prime numbers
-                gcd
-                lcm""")
-
-        if command == 'multiplication':
-            current_number = 0
-            amount = int(input('How many numbers do you want to multiply? '))
-            result = 1
-            for y in range(amount):
-                current_number += 1
-                number = int(input(f'Enter number {current_number}: '))
-                result *= number
-            print(f'Your multiplication result is {result}')
-        elif command == 'subtraction':
-            a = float(input('Enter the minuend: '))
-            b = float(input('Enter the subtrahend: '))
-            difference = subtraction(a, b)
-            print(f'The result of subtraction is {difference}')
-        elif command == 'addition':
-            a = int(input('how many numbers do you want to add together? '))
-            b = float(input('write the 1. number '))
-            for x in range(1, a):
-
-                c = float(input(f'write the {x+1}. number '))
-                b += c
-            print(f'the result is {b} ')
+            if command == 'multiplication':
+                current_number = 0
+                amount = int(input('How many numbers do you want to multiply? '))
+                result = 1
+                for y in range(amount):
+                    current_number += 1
+                    number = int(input(f'Enter number {current_number}: '))
+                    result *= number
+                print(f'Your multiplication result is {result}')
+            elif command == 'subtraction':
+                a = int(input("how many numbers do you want to substract from another?"))
+                result = float(input("enter the minuend"))
+                for x in range(a):
+                    c = float(input(f"write the {x+1}. subtrahend"))
+                    result -= c
 
 
+                print(f'The result of subtraction is {result}')
+            elif command == 'addition':
+                a = int(input('how many numbers do you want to add together? '))
+                result = float(input('write the 1. number '))
+                for x in range(1, a):
+                    c = float(input(f'write the {x+1}. number '))
+                    result += c
+                print(f'the result is {result} ')
+            elif command == "division":
+                amount = int(input('Enter how many numbers you want to divide by this number: '))
+                result = float(input('Enter the divisor: '))
+                z = 0
+                for x in range(1, amount+1):
+                    z += 1
+                    y = float(input(f'write a {z}. number that you want to devide by '))
+                    if y==0:
+                        sys.exit("nawet nie probuj gagatku")
+                    result /= y
+                    print(f'The result of the division is: {result}')
 
-
-        elif command == "division":
-            divisor = float(input('Enter the divisor: '))
-            amount = int(input('Enter how many numbers you want to divide by this number: '))
-            z = 0
-            for x in range(1, amount+1):
-                z += 1
-                y = float(input(f'write a {z}. number that you want to devide by '))
-                if y==0:
-
-                    sys.exit("nawet nie probuj gagatku")
-                    divisor /= y
-                    print(f'The result of the division is: {divisor}')
-    else:
-        print('Something is wrong with the command')
+            elif command == "exponentiation":
+                result = float(input("Write a base"))
+                b = float(input("write a power"))
+                result **= b
+                print(f"the result is:{result}")
+        else:
+            print('Something is wrong with the command')
     answer = ''
-    answer = input('Do you want to continue? ')
+    secque = ''
+    answer = input('Do you want to continue(yes or not)? ')
+    if answer == "yes":
+        secque = input("Do you want to continue with your result (yes) or you want to start again (no)")
+    if secque =="yes":
+        command = input('write a command (if you dont know any commands write "?" ')
+        if command in commands:
+            if command == "?":
+                print("""
+                           addition
+                           subtraction
+                           multiplication
+                           division
+                           exponentiation
+                           root extraction
+                           percentage calculation
+                           logarithms
+                           intersection point of two lines
+                           prime numbers
+                           gcd
+                           lcm""")
+
+            if command == 'multiplication':
+                current_number = 0
+                amount = int(input('How many numbers do you want to multiply this number? '))
+                for y in range(amount):
+                    current_number += 1
+                    number = int(input(f'Enter number {current_number}: '))
+                    result *= number
+                print(f'Your multiplication result is {result}')
+            elif command == 'subtraction':
+                a = int(input("How many numbers you want to substract? "))
+                current_number = 0
+                for x in range(1, a):
+                    current_number += 1
+                    b = float(f"write the {current_number}. number you want to substract ")
+                    result -= b
+                print(f'The result of subtraction is {result}')
+            elif command == 'addition':
+                a = int(input('how many numbers do you want to add together? '))
+                for x in range(1, a):
+                    c = float(input(f'write the {x + 1}. number '))
+                    result += c
+                print(f'the result is {result} ')
+            elif command == "division":
+                amount = int(input('Enter how many numbers you want to divide by this number: '))
+                z = 0
+                for x in range(1, amount + 1):
+                    z += 1
+                    y = float(input(f'write a {z}. number that you want to devide by '))
+                    result /= y
+                    if y == 0:
+                        sys.exit("nawet nie probuj gagatku")
+                    print(f'The result of the division is: {result}')
+            elif command == "exponentiation":
+                a = float(input("Write a base"))
+                b = float(input("write a power"))
+                result = exponentiation(a, b)
+                print(f"the result is:{result}")
+        else:
+            print('Something is wrong with the command')
+
+
+
 
 input('Press enter to exit')
+
+# zostało         exponentiation
+#                 root extraction
+#                 percentage calculation
+#                 logarithms
+#                 intersection point of two lines
+#                 prime numbers
+#                 gcd
+#                 lcm
+
 
 
 
