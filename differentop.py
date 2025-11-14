b3285b28b177d292c52b741f6b50537e4c125489
Alan38345 <alaxddalan@gmail.com>
2025-11-14 16:58:31 +0100

    naprawa

--- differentop.py
+++ differentop.py
@@ -1,8 +1,8 @@
 def op(a, b):
     amount = int(input("How many numbers does your action have? "))
+    result = b
     for x in range(amount):
         numbers = float(input(f"Write the {x+1}. number: "))
-        result = b
         if a == '*':
             result *= numbers
 
